832b6e511949b6b1eb2a710368bf9907856852f1
uttamrokzz <ugrokzz@gmail.com>
2026-09-25 23:24:18 +0530

    Update routes/learning.py

--- routes/learning.py
+++ routes/learning.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
-from core import page
+from core import page, nav
 
 bp = Blueprint("learning", __name__)
 
-
 @bp.route("/learning")
 def learning():
-    return page("""
-      <div class="top-bar"><h2>Learning Database</h2>
-        <a class="btn btn-secondary" href="/">&larr; Home</a></div>
-      <div class="card">
-        <p class="name">Coming soon</p>
-        <p class="muted">This is where you'll collect rules, principles,
-        and textbook knowledge &mdash; separate from your personal research.</p>
-      </div>
-    """)
+    h = '<div class="top-bar"><h2>Learning Database</h2>' + nav("/") + "</div>"
+    h += "<div class='card'><p class='name'>Coming soon</p>"
+    h += "<p class='muted'>Collect rules, principles, textbook knowledge here.</p></div>"
+    return page(h)
