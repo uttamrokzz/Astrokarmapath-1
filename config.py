d3d6e6dcdbacf2f1e7cee01f8ec1cd7c9e077bd6
uttamrokzz <ugrokzz@gmail.com>
2026-09-26 16:58:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+# -*- coding: utf-8 -*-
 import os
 
-URL   = os.environ.get("TURSO_DATABASE_URL",
-        "libsql://astrokarmapath-astrokarmapath.aws-ap-south-1.turso.io")
-TOKEN = os.environ.get("TURSO_AUTH_TOKEN", "")
+URL = os.environ.get('TURSO_DATABASE_URL',
+    'libsql://astrokarmapath-astrokarmapath.aws-ap-south-1.turso.io')
+TOKEN = os.environ.get('TURSO_AUTH_TOKEN', '')
+
+try:
+    from local_config import TOKEN as _LOCAL
+    if _LOCAL:
+        TOKEN = _LOCAL
+except ImportError:
+    pass
